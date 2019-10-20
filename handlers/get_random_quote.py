aed58a70fbfd6d7dd4180d31ada92bebc187fef0
Dimas Ari <dimasinchidi@gmail.com>
2019-10-21 06:16:46 +0700

    feat: get random quote using peewee random function.

--- handlers/get_random_quote.py
+++ handlers/get_random_quote.py
@@ -1,2 +1,13 @@
+from peewee import fn
+
+from models import Quote
+
+
 def handler_get_random_quote(update, context):
-    update.message.reply_text('Random quote retrieved!')
+    chat = update.effective_chat
+    if Quote.select().where(Quote.chat_id == chat.id).count():
+        selected_quote = Quote.select().where(Quote.chat_id == chat.id).order_by(fn.Random()).limit(1)
+        # TODO forward the quoted message
+        update.message.reply_text(f'under construction {selected_quote.stored_message_id}.')
+        return
+    update.message.reply_text('Please add at least one quote first.')
